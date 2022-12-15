533d164a7d21684488be10fef7ff65bca6a717d6
Alesker <alex.alesker@gmail.com>
2022-12-15 21:54:19 +0300

    Console UI is added

---
+++ ExpressionCalcCons/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExpressionCalc {
+
+internal class Program {
+    static void Main() {
+        Console.WriteLine("Enter the expression OR");
+        Console.WriteLine("Press <Enter> to cancel processing");
+        while (true) {
+            var s = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(s)) {
+                break;
+            }
+
+            Console.WriteLine(ExpressionCalc.Calculate(s));
+        }
+    }
+}
+}
